0730e075911d5bc909dfffec3d611a20ddd820ab
omerunsal <47754244+omerunsal@users.noreply.github.com>
2024-12-02 22:33:23 +0300

    Initial Commit

---
+++ MagiCo/MagiCoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagiCoApp.swift
+//  MagiCo
+//
+//  Created by omer on 2.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct MagiCoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
